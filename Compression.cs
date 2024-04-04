7cda8f74781b79af38ace6bcda80d0bf4147d91d
Zoltán Zörgő <zoltan.zorgo@gmail.com>
2024-04-04 23:31:24 +0200

    Add project files.

---
+++ Compression.cs
@@ -0,0 +1,61 @@
+﻿using System.IO.Compression;
+
+namespace PreCompress;
+
+internal static class Compression
+{
+    public static async Task CompressAsync(FileInfo file, bool bratli, bool gzip, DirectoryInfo? destination, CancellationToken token)
+    {
+        var tasks = new List<Task>();
+
+        var dest = Path.Combine(destination?.FullName ?? Path.GetDirectoryName(file.FullName)!, Path.GetFileName(file.Name));
+
+        if (bratli)
+        {
+            tasks.Add(CompressFileAsync(file.FullName, dest + ".br", BrotliCompressor, token));
+        }
+
+        if (gzip)
+        {
+            tasks.Add(CompressFileAsync(file.FullName, dest + ".gz", GZipCompressor, token));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+    
+    private static CompressionLevel GetCompressionLevel()
+    {
+        if (Enum.IsDefined(typeof(CompressionLevel), 3)) // NOTE: CompressionLevel.SmallestSize == 3 is not supported in .NET Core 3.1
+        {
+            return (CompressionLevel)3;
+        }
+        return CompressionLevel.Optimal;
+    }
+
+    private static async Task CompressFileAsync(string originalFileName, string compressedFileName, Func<Stream, Stream, CancellationToken, Task> compressor, CancellationToken cancel = default)
+    {
+        using (FileStream originalStream = File.Open(originalFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            using (FileStream compressedStream = File.Create(compressedFileName))
+            {
+                await compressor(originalStream, compressedStream, cancel);
+            }
+        }
+    }
+
+    private static async Task BrotliCompressor(Stream originalStream, Stream compressedStream, CancellationToken cancel = default)
+    {
+        using (var compressor = new BrotliStream(compressedStream, GetCompressionLevel()))
+        {
+            await originalStream.CopyToAsync(compressor, cancel);
+        }
+    }
+
+    private static async Task GZipCompressor(Stream originalStream, Stream compressedStream, CancellationToken cancel = default)
+    {
+        using (var compressor = new GZipStream(compressedStream, GetCompressionLevel()))
+        {
+            await originalStream.CopyToAsync(compressor, cancel);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+using PreCompress;
+
+var fileOption = new Option<FileInfo[]?>(
+    name: "-i", 
+    description: "The file to compress.",
+    isDefault: true,
+    parseArgument: result =>
+    {
+        if (result.Tokens.Count == 0)
+        {
+            result.ErrorMessage = "No input sepcified";
+            return null;
+        }
+
+        char[] wildcards = [ '*', '?' ] ;
+
+        var res = result.Tokens
+            .Select(x => (pattern: x.Value, dir: EmptyCoalesce(Path.GetDirectoryName(x.Value), Directory.GetCurrentDirectory())!))
+            .SelectMany(x =>
+            x switch
+            {
+                _ when Directory.Exists(x.pattern) => 
+                    new DirectoryInfo(x.pattern).GetFiles(),
+                _ when x.pattern.Intersect(wildcards).Any() && Directory.Exists(x.dir) => 
+                    Directory.GetFiles(x.dir, Path.GetFileName(x.pattern)).Select(f => new FileInfo(f)),
+                _ when File.Exists(x.pattern) => 
+                    [ new FileInfo(x.pattern) ],
+                _ => 
+                    []
+            }
+        ).ToArray();
+
+        if(res.Length == 0)
+        {
+            result.ErrorMessage = "No files found";
+            return null;
+        }
+
+        return res;
+    })
+{
+    AllowMultipleArgumentsPerToken = true,
+    IsRequired = true,
+};
+
+fileOption.AddAlias("--input");
+
+var bratliOption = new Option<bool>(name: "-nb", description: "Omit Bratli compressed version.", getDefaultValue: () => false);
+bratliOption.AddAlias("--NoBratli");
+
+var gzipOption = new Option<bool>(name: "-ng", description: "Omit GZip compressed version.", getDefaultValue: () => false);
+gzipOption.AddAlias("--NoGzip");
+
+var destinationOption = new Option<DirectoryInfo?>(name: "-d", description: "The output directory.");
+destinationOption.AddAlias("--destination");
+
+var rootCommand = new RootCommand("PreCompression tool for ASP.NET Core");
+rootCommand.AddOption(fileOption);
+rootCommand.AddOption(bratliOption);
+rootCommand.AddOption(gzipOption);
+rootCommand.AddOption(destinationOption);
+rootCommand.SetHandler(ReCompress, fileOption, bratliOption, gzipOption, destinationOption);
+
+return await rootCommand.InvokeAsync(args);
+
+static async Task ReCompress(FileInfo[]? files, bool omitBratli, bool omitGZip, DirectoryInfo? destination)
+{
+    if(files is null) return;
+
+    foreach (var file in files)
+    {
+        try
+        {
+            Console.WriteLine($"Processing {file.FullName}");
+            await Compression.CompressAsync(file, !omitBratli, !omitGZip, destination, default);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Error processing {file.FullName}: {ex.Message}");
+        }
+        
+    }
+}
+
+static string? EmptyCoalesce(params string?[] values) => values.FirstOrDefault(x => !string.IsNullOrWhiteSpace(x));
